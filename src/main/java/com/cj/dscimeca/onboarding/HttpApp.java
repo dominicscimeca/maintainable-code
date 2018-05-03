7772a0a1966dc1283f4ba919b83bfc0938ff2986
dscimeca <dscimeca@cj.com>
2018-05-03 15:20:05 -0700

    very basic http server (zero feature release)

---
+++ src/main/java/com/cj/dscimeca/onboarding/HttpApp.java
@@ -0,0 +1,16 @@
+package com.cj.dscimeca.onboarding;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HttpApp extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello, world");
+    }
+}
